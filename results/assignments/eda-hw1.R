4e10b32989a8a0706ab2bc4ff9096cd32f990c60
Blue Octopus <visual.eyes.success@gmail.com>
2020-04-07 00:19:40 +0200

    couldnt see commented lines which is where i put my answers. Edited to show text

--- results/assignments/eda-hw1.R
+++ results/assignments/eda-hw1.R
@@ -16,31 +16,39 @@ multiplyXandY <- function(x, y){x*y}
 results <- multiplyXandY(temps,weights)
 
 #Homework-Explorer questions
-# #1 -area Rhine catchment=186,000km2 area/1220km length/within 9countries
-# #2 - make a code for the entire catchment to take on 5mm/hr for 24hours
-# #3 -how much increase in the average river runoff 
-#how to solve
-#how to calculate brainstorm=> Vol : Area : +AveRunoff 
-#hunch says check the dirs for tables, possible to add 24*5mm to ea day, and ave this 
-#utilize col select 
+ #1 -area Rhine catchment=186,000km2 area/1220km length/within 9countries
+ #2 - make a code for the entire catchment to take on 5mm/hr for 24hours
+ #3 -how much increase in the average river runoff 
+how to solve
+how to calculate brainstorm=> Vol : Area : +AveRunoff 
+hunch says check the dirs for tables, possible to add 24*5mm to ea day, and ave this 
+
 mmPerHour <- c(5)
 hoursPerDay <- c(24)
 daysPerYear <- c(365)
 areaCatchmentKm2 <- c(186000)
 volumeIncrease <- c(mmPerHour*hoursPerDay*daysPerYear*areaCatchmentKm2)
-#i have no idea how to calculate this,and feel this is absolutely incorrect
 volumeIncrease
 
+---i have no idea how to calculate this,and feel this is absolutely incorrect
+
+
 #4a
-#low flow was not mentioned 
+a4 <- c("low flow was not mentioned")
 #4b
-#winter tourism/agricultural irrigation supply/industry depending on Rhine
+b4 <- c("winter tourism/agricultural irrigation supply/industry depending on Rhine")
 #4c
-#The Rhine affects upwards of 50million people just in its basin, and is a major source of livlihood for multitudes of people. They wrote this to bring awareness to the humans impact on Earth is what I believe. Also considering they were concerned with preserving the transportation routes and ports along the shores. 
+c4 <- c("The Rhine affects upwards of 50million people just in its basin, and is a major source of livlihood for multitudes of people. They wrote this to bring awareness to the humans impact on Earth is what I believe. Also considering they were concerned with preserving the transportation routes and ports along the shores.")
 #4d
-#I haven't found any online yet
+d4 <- c("I haven't found any online yet")
 #4e
-#Spring 2016 seems to be the high flow event since year 2000
+e4 <- c("Spring 2016 seems to be the high flow event since year 2000")
+
+a4
+b4
+c4
+d4
+e4
 
 
 
