739b4955081356cb32ab627e352d7deac54f272f
Blue Octopus <visual.eyes.success@gmail.com>
2020-04-07 00:14:05 +0200

    Commit HW1&Hw2 (incomplete!)

--- code/00_init.R
+++ code/00_init.R
@@ -3,3 +3,11 @@ dir.create('./data/raw')
 dir.create('./results')
 dir.create('./results/figures')
 dir.create('./docs')
+getwd()
+
+Temperatures= c(3,6,10,14)
+WEIGHTS = c(1,0.8,1.2,1)
+library(data.table)
+aa=function(x, y)
+{x*y}
+results <- aa(Temperatures,WEIGHTS)
--- results/assignments/eda-hw1.R
+++ results/assignments/eda-hw1.R
@@ -5,10 +5,42 @@ dir.create('./results/figures')
 dir.create('./docs')
 getwd()
 
-Temperatures= c(3,6,10,14)
-WEIGHTS = c(1,0.8,1.2,1)
+
+
+temps <- c(3,6,10,14) #celcius
+weights <- c(1.0,0.8,1.2,1.0) #kg
 library(data.table)
-aa=function(x, y)
-{x*y}
-results <- aa(Temperatures,WEIGHTS)
+#create function
+multiplyXandY <- function(x, y){x*y}
+#call function passing temps and weights as args
+results <- multiplyXandY(temps,weights)
+
+#Homework-Explorer questions
+# #1 -area Rhine catchment=186,000km2 area/1220km length/within 9countries
+# #2 - make a code for the entire catchment to take on 5mm/hr for 24hours
+# #3 -how much increase in the average river runoff 
+#how to solve
+#how to calculate brainstorm=> Vol : Area : +AveRunoff 
+#hunch says check the dirs for tables, possible to add 24*5mm to ea day, and ave this 
+#utilize col select 
+mmPerHour <- c(5)
+hoursPerDay <- c(24)
+daysPerYear <- c(365)
+areaCatchmentKm2 <- c(186000)
+volumeIncrease <- c(mmPerHour*hoursPerDay*daysPerYear*areaCatchmentKm2)
+#i have no idea how to calculate this,and feel this is absolutely incorrect
+volumeIncrease
+
+#4a
+#low flow was not mentioned 
+#4b
+#winter tourism/agricultural irrigation supply/industry depending on Rhine
+#4c
+#The Rhine affects upwards of 50million people just in its basin, and is a major source of livlihood for multitudes of people. They wrote this to bring awareness to the humans impact on Earth is what I believe. Also considering they were concerned with preserving the transportation routes and ports along the shores. 
+#4d
+#I haven't found any online yet
+#4e
+#Spring 2016 seems to be the high flow event since year 2000
+
+
 
