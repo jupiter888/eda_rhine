06b4349bc0e1501c1937420bb5297334e8891bcb
Blue Octopus <visual.eyes.success@gmail.com>
2020-04-08 00:38:42 +0200

    Day 2 tasks-Part 1
    we took csv and raw data files, transformed everything to data.table objects,structured tables,  saved as rds formatted files, checked for redundancies, abbreviated names, removed decimal commas for readability.

---
+++ code/01a_import_runoff_info.R
@@ -0,0 +1,16 @@
+#Day 2 EDA Hw2 Part1 
+#Daniel Tracy
+
+library(data.table)
+list.files('data/raw')
+runoff_stations <- fread('./data/raw/runoff_stations.csv')
+head(runoff_stations)
+tail(runoff_stations)
+str(runoff_stations)
+runoff_stations$station
+runoff_stations[, sname := factor(abbreviate(station))]
+runoff_stations[, id := factor(id)]
+runoff_stations[, lat := round(lat, 3)]
+runoff_stations[, lon := round(lon, 3)]
+runoff_stations[, altitude := round(altitude, 0)]
+saveRDS(runoff_stations, './data/runoff_stations_raw.rds')
