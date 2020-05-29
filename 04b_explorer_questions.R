5238179c2095ecd3f65c916f4da3c53e95705d87
Blue Octopus <visual.eyes.success@gmail.com>
2020-05-29 23:59:15 +0200

    progress.

---
+++ 04b_explorer_questions.R
@@ -0,0 +1,11 @@
+#Explorers Q's ch.4
+#1 Differnece between median and 0.5 quantile
+#In statistics, the quantiles slice points which are the dividing of probablity distribution. The second quatile, aka divided into two parts, aka 0.5 of the whole,  is the same as the median. 
+
+#2Why runoff medians lower than the mean at ea station
+#median is not influenced by positive outliers. These extrapolations are influencing the mean, 
+#because of the values added to the sum before dividing by the 
+#nth value of the set the mean is taken from. 
+
+#3notice something strange w loc of the stations LOBI and REES, possible explanation
+ 
