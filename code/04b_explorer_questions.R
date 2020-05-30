21c4d08a56f2a4d6d73f8c9a31537df0ac8f7db1
Blue Octopus <visual.eyes.success@gmail.com>
2020-05-30 19:34:22 +0200

    setting up solution for complex question

--- code/04b_explorer_questions.R
+++ code/04b_explorer_questions.R
@@ -11,6 +11,45 @@
 #Yes I can see two observations. These stations have the most pronounced valley signifying end of hydrological cycle in the 10th month of the year. Also, I see varying altitude difference with the altitude slightly higher, yes closer to the sea yet higher. This is very peculiar given that Netherlands is under sea-level primarily. 
 
 #4Which months, seasons, years with the highest/lowest runoff at ea loc
+# library
+source('./code/source_code.R')
+#my plot---------X AXIS RUNOFF MEAN----
+#Y AXIS MONTH(A)/SEASON(B)/YEAR(C) ----three graphs
+#---------------------------------------------------
+# PLOT A------ Runoff mean of Months---- using runoff_(month) from 03_preparation
+mo_min <- runoff_month[, min(value), by=sname]
+mo_max <- runoff_month[, max(value), by=sname]
 
+
+
+
+
+
+
+
+
+#PLOT B------ Season-----W=winter / S=summer
+w_min <- 
+w_max <- 
+s_min <- 
+s_max <-
+  
+#PLOT C----- YEAR=yr  
+yr_min <- runoff_year[, min(value), by=sname]
+yr_max <- runoff_year[, max(value), by=sname]
+ 
+
+#---------------------------------------------------
+# Example PLOT
+ggplot(lincoln_weather, aes(x = `Mean Temperature [F]`, y = `Month`, fill = ..x..)) +
+  geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01) +
+  scale_fill_viridis(name = "Temp. [F]", option = "C") +
+  labs(title = 'Temperatures in Lincoln NE in 2016') +
+  theme_ipsum() +
+  theme(
+    legend.position="none",
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8)
+  )
 
 
